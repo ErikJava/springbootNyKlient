c5ad408dfc6cecc56cea706f55a782020ab1dfed
ErikJava <zycore90@gmail.com>
2023-03-31 12:29:08 +0200

    Added comments.

--- src/main/java/com/springboot/springboot/Client.java
+++ src/main/java/com/springboot/springboot/Client.java
@@ -6,16 +6,23 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Scanner;
 
 public class Client {
+
+    // URL of the REST API.
     private static final String SERVER_URL = "http://localhost:8080/json";
 
     public static void main(String[] args)  {
+
+        // Prints messages to console.
         System.out.println("Connection Successful! \n");
         System.out.println("Instructions: \nIf you'd like to receive a JSON-Object, enter '1' & then enter the name of the object you'd like to receive.");
         System.out.println("If you'd like to receive the whole JSON-File, enter '2'.");
         System.out.println("To return to the main menu after receiving a JSON-Object, enter 'n' & then 'y'.\n");
+
+        // Scanner for taking user input.
         Scanner scanner = new Scanner(System.in);
         String input;
 
+        // Repeats until the user decides to exit.
         do {
             System.out.println("What would you like to receive?");
             System.out.println("1. A JSON-Object");
@@ -29,6 +36,7 @@ public class Client {
                     System.out.println("\nEnter name of JSON-Object to receive (object1/object2/object3)");
                     objName = scanner.nextLine();
 
+                    // Sends GET request to REST API to retrieve the JSON object.
                     RestTemplate restTemplate = new RestTemplate();
 
                     UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(SERVER_URL)
@@ -36,6 +44,8 @@ public class Client {
 
                     ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
 
+                    // If the JSON object is not found, print error message to console.
+                    // Otherwise, print the JSON object to console.
                     String jsonString = response.getBody();
                     if (jsonString == null || jsonString.equals("null")) {
                         System.out.println("ERROR: JSON-Object not found.");
@@ -47,6 +57,7 @@ public class Client {
                     input = scanner.nextLine();
                 } while (input.equalsIgnoreCase("y"));
             } else if (input.equals("2")) {
+                // Sends GET request to REST API to retrieve the whole JSON file.
                 RestTemplate restTemplate = new RestTemplate();
 
                 UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(SERVER_URL)
@@ -54,6 +65,7 @@ public class Client {
 
                 ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
 
+                // Prints the JSON file to console.
                 String jsonString = response.getBody();
                 System.out.println("JSON File:\n" + jsonString);
             } else if (input.equalsIgnoreCase("exit")) {
